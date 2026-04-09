0b309436b9a964b2e1d719366440274fcfc19dc2
Theredrat <sthadam7@gmail.com>
2026-04-10 00:12:21 +0545

    Add NEPSE Dashboard to display stock data

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+st.title("📊 NEPSE Dashboard")
+
+def get_data():
+try:
+url = "https://www.nepalstock.com/api/nots/securityDailyTradeStat/58"
+headers = {"User-Agent": "Mozilla/5.0"}
+res = requests.get(url, headers=headers)
+return pd.DataFrame(res.json())
+except:
+return pd.DataFrame()
+
+df = get_data()
+
+if df.empty:
+st.write("Data not loading")
+else:
+st.dataframe(df.head(20))
