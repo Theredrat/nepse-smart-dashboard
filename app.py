1e29c94b96952825e6c3f7776e0cc68c80980eea
Theredrat <sthadam7@gmail.com>
2026-04-10 00:24:37 +0545

    Improve error handling and update messages

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ try:
 url = "https://www.nepalstock.com/api/nots/securityDailyTradeStat/58"
 headers = {"User-Agent": "Mozilla/5.0"}
 res = requests.get(url, headers=headers)
-return pd.DataFrame(res.json())
+data = res.json()
+return pd.DataFrame(data)
 except:
 return pd.DataFrame()
 
 df = get_data()
 
 if df.empty:
-st.write("Data not loading")
+st.write("❌ Data not loading")
 else:
 st.dataframe(df.head(20))
